fee4e509ae1ac2d7cb4689ae2a20b168a4dbadd2
IBteesam <83177878+ibteesam@users.noreply.github.com>
2021-11-12 19:10:11 +0700

    New
    Script

---
+++ ท่าทาง7.lua
@@ -0,0 +1,386 @@
+function START()
+menu = gg.choice({
+	"ท่าใหม่ New 🐲🐍🦎🐬🐋🐳🦈",
+	"ท่าปื่นLevel7🐝🦐🐞🌹🐙🌺",
+    "ออกสคริป 😎🏉🔇"},nil,"🅵🆄🅲🅺 ​ 🆈🅾🆄 ​ 🅼🅰🅽")
+	
+if menu == 1 then new() end
+if menu == 2 then level() end
+if menu == 3 then exit() end
+LOOP = -1 end
+
+function level()
+function STAR()
+menu = gg.choice({
+   "ท่าAK💦☠️",
+   "ปิด🔴",
+   "ท่า Scar💢 🌺",
+   "ปิด🔴",
+   "ท่า Xm8🌧️🌊",
+   "ปิด🔴",
+   "ท่าลูกซอง🍏🍀",
+   "ปิด🔴",
+   "ท่าMp40🏵️🐯",
+   "ปิด🔴",
+   "ออกสคริป"},nil,"🔰✅ท่า EVO Level 7 Script 🔰✅ ")
+   
+if menu == 1 then Ak() end
+if menu == 2 then o()end
+if menu == 3 then Scar()end
+if menu == 4 then of() end
+if menu == 5 then Xm8() end
+if menu == 6 then off()end
+if menu == 7 then m188() end
+if menu == 8 then offf() end
+if menu == 9 then mp40() end
+if menu == 10 then offff()end
+if menu == 11 then xit()end
+LOO = -1
+end
+function Ak()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,063",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("🔥เปิดสคริปเรียบร้อย !🔥")
+end
+
+function o()
+gg.searchNumber("909,000,063",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดสคริป🔴❗ ")
+end
+function Scar()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,068",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("🔥เปิดสคริปเรียบร้อย !🔥")
+end
+function of()
+gg.searchNumber("909,000,068",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดสคริป🔴❗ ")
+end
+function Xm8()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,085",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("🔥เปิดสคริปเรียบร้อย !🔥")
+end
+function off()
+gg.searchNumber("909,000,085",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดสคริป🔴❗ ")
+end
+function m188()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,081",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("🔥เปิดสคริปเรียบร้อย !🔥")
+end
+function offf()
+gg.searchNumber("909,000,081",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดสคริป🔴❗ ")
+end
+
+function mp40()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,075",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("🔥เปิดสคริปเรียบร้อย !🔥")
+end
+function offff()
+gg.searchNumber("909,000,075",gg.TYPE_DWORD) 
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดสคริป🔴❗ ")
+end
+function xit()
+START()
+
+end
+
+while true do
+if gg.isVisible(true) then
+LOO = 1
+gg.setVisible(false) 
+end
+gg.clearResults()
+if LOO == 1then
+STAR()
+end
+end
+end
+
+
+
+function new()
+function TART()
+menu = gg.choice({
+	"ท่าปักธงแคลน🚩😯",
+	"ปิด✘",
+	"ท่าเตะบาสสเก็ตบอล🏀😮",
+	"ปิด✘",
+	"ท่าUmp😕🍭",
+	"ปิด✘",
+    "ท่า กากบาด😰✘",
+    "ปิด✘",
+    "ท่าใส่แว่น😎💐",
+    "ปิด✘",
+    "ท่าบีบอย🎻🔊",
+    "ปิด✘",
+    "ท่าบิกบอกซ์🔊🎶",
+    "ปิด✘" ,
+    "ท่าต่อยใหม่🥊🎯",
+    "ปิด✘",
+    "ท่าบาสสเก็ตบอล🏀🏅",
+    "ปิด✘",
+    "ออกสคริป😊☹️ " },nil,"♣️♠️🎲new script ท่า ใหม่♠️ 🔫")
+	
+if menu == 1 then cleaner() end
+if menu == 2 then o() end
+if menu == 3 then bass() end
+if menu == 4 then of() end
+if menu == 5 then ump() end
+if menu == 6 then off() end
+if menu == 7 then kabad() end
+if menu == 8 then offf() end
+if menu == 9 then wen() end
+if menu == 10 then offff() end
+if menu == 11 then beboy() end
+if menu == 12 then offfff() end
+if menu == 13 then bixbox() end
+if menu == 14 then offffff() end
+if menu== 15 then toy() end
+if menu == 16 then offfffff() end
+if menu == 17 then sket() end
+if menu == 18 then offffffff() end
+if menu == 19 then exi() end
+OOP = -1
+end
+function cleaner()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,128",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าปักธงแคลนแล้วค้าบ✔😊")
+end
+
+function o()
+gg.searchNumber("909,000,128",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าปักธงแคลนแล้วค้าบ😰✘")
+end
+
+function bass()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,124",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าเตะบาสสเก็ตบอลแล้วค้าบ✔😊")
+end
+
+function of()
+gg.searchNumber("909,000,124",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าเตะบาสสเก็ตบอลแล้วค้าบ😰✘")
+end
+
+function ump()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,098",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าUMPแล้วค้าบ😊✔")
+end
+
+function off()
+gg.searchNumber("909,000,098",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าUMPแล้วค้าบ✘😧")
+end
+
+function kabad()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,097",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่ากากบาดแล้วค้าบ✔😊")
+end
+
+function offf()
+gg.searchNumber("909,000,097",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่ากากบาดแล้วค้าบ😰✘")
+end
+
+function wen()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,123",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าใส่แว่นแล้วค้าบ✔😊")
+end
+
+function offff()
+gg.searchNumber("909,000,123",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าใส่แว่นแล้วค้าบ😧✘")
+end
+
+
+function beboy()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,125",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าบีบอยแล้วค้าบ😊✔")
+end
+
+
+function offfff()
+gg.searchNumber("909,000,125",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าบีบอยแล้วค้าบ😧✘")
+end
+
+
+function bixbox()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,126",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าบิกบอกซ์แล้วค้าบ😊✔")
+end
+
+function offffff()
+gg.searchNumber("909,000,126",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าบิกบอกซ์แล้วค้าบ😧✘")
+end
+
+function toy()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,127",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าต่อยแบบใหม่แล้วแล้วค้าบ😊✔")
+end
+
+function offfffff()
+gg.searchNumber("909,000,127",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าต่อยแบบใหม่แล้วแล้วค้าบ😰✘")
+end
+
+
+function sket()
+gg.searchNumber("909,000,002",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,121",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("เปิดท่าบาสเสก็ตบอลแล้วแล้วค้าบ😊✔")
+end
+
+
+function offffffff()
+gg.searchNumber("909,000,121",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("909,000,002",gg.TYPE_DWORD) 
+gg.clearResults()
+gg.toast("ปิดท่าบาสสเก็ตบอลแล้วค้าบแล้วแล้วค้าบ✘😧")
+end
+
+function exi()
+START()
+end
+
+
+
+
+
+
+
+
+
+
+
+
+while true do
+if gg.isVisible(true) then
+OOP = 1
+gg.setVisible(false) 
+end
+gg.clearResults()
+if OOP == 1then
+TART()
+end
+end
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+function exit()
+gg.alert("🄶🄾🄾🄳 ​ 🄻🅄🄺🄺🅈")
+print("𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃𝖃")
+os.exit()
+end
+
+
+
+
+
+
+
+while true do
+if gg.isVisible(true) then
+LOOP = 1
+gg.setVisible(false) 
+end
+gg.clearResults()
+if LOOP == 1then
+START()
+end
+end
